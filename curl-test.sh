d5bc2ce590eceaffa87523a54d92e72d8c450528
Ariel Couso <ariel.couso@gmail.com>
2021-12-14 00:20:22 -0300

    adding curl script

---
+++ curl-test.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -X POST -H "Content-Type: text/plain" -d '(1.115,2.119), (1.108,2.220), (1.101,2.209), (1.110,2.209), (1.112,2.212)' http://localhost:5000/calculate
--- docker-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sudo -i docker build -t arcsurfing/devopsapp:v0.0.1 .
