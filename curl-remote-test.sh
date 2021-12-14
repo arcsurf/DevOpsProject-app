99a7fa102b9374bed8406943258e339c0cca194b
Ariel Couso <ariel.couso@gmail.com>
2021-12-14 13:36:45 -0300

    Create curl-remote-test.sh

---
+++ curl-remote-test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sleep 1
+curl -X POST -H "Content-Type: text/plain" -d '(1.115,2.119), (1.108,2.220), (1.101,2.209), (1.110,2.209), (1.112,2.212)' http://app.couso.com.ar:5001/calculate
