3ed18c02acfaf2bc3b4576d120eb809b2bbfbff2
Ariel Couso <ariel.couso@gmail.com>
2021-12-14 04:20:02 -0300

    switching back to original docker script.

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-ssh ubuntu@app.couso.com.ar 'ls -la'
+docker -H ssh://ubuntu@app.couso.com.ar rm -f app_prod
+docker -H ssh://ubuntu@app.couso.com.ar run -d -p 5000:5000 --name app_prod arcsurfing/devopsapp:v0.0.4
